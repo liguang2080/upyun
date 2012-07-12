7eed3a274f7c1c98227b87cc8b1973cba24f1b46
lg2046 <lg2046@gmail.com>
2012-07-12 22:41:12 +0800

    bump 0.2.0

--- lib/upyun.rb
+++ lib/upyun.rb
@@ -1,2 +1,2 @@
-require File.dirname(__FILE__) + "/upyun/version"
-require  File.dirname(__FILE__) + "/upyun/bucket"
+require File.expand_path(File.dirname(__FILE__) + "/upyun/version")
+require File.expand_path(File.dirname(__FILE__) + "/upyun/bucket")
