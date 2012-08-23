fa06087a6c4c664f477d3abd148aab3aed288764
lg2046 <lg2046@gmail.com>
2012-08-23 15:56:15 +0800

    bucket_name

--- lib/upyun.rb
+++ lib/upyun.rb
@@ -4,6 +4,6 @@ require File.expand_path(File.dirname(__FILE__) + "/upyun/bucket")
 
 module Upyun
   def self.get_config_for_bucket(bucket)
-    UP_BUCKETS.find {|k, v| v.bucket_name == bucket}.try(:last)
+    UP_BUCKETS.find {|k, v| v.bucket_name.to_s == bucket.to_s}.try(:last)
   end
 end
