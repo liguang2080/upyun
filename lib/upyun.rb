e147c9ec95af2293a64a2045d7d645b224e0ecbd
lg2046 <lg2046@gmail.com>
2012-08-24 14:18:24 +0800

    add rand save path

--- lib/upyun.rb
+++ lib/upyun.rb
@@ -6,4 +6,9 @@ module Upyun
   def self.get_config_for_bucket(bucket)
     UP_BUCKETS.find {|k, v| v.bucket_name.to_s == bucket.to_s}.try(:last)
   end
+  
+  # 为给定的路径与文件名生成保存后的远程路径
+  def self.rand_save_path(filename)
+    "/#{Date.today.year}/#{'%2s' % Date.today.month.to_s.rjust(2, "0")}/#{RandomCode.mix_string(16).downcase}#{File.basename(filename)}"
+  end
 end
